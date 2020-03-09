891624eb429ad5c377d422b243109bb2ef58006e
nicomancer <pellizzon.nicole@gmail.com>
2020-03-09 16:43:11 +0000

    set up Poisson class
    everything setup for solving the linear equations

--- LidDrivenCavitySolver/PoissonSolver.cpp
+++ LidDrivenCavitySolver/PoissonSolver.cpp
@@ -10,7 +10,7 @@ PoissonSolver::~PoissonSolver(){
 
 
 //MEMBER FUNCTIONS
-void SetA(int Nx, int Ny, double*a, double deltaX, double deltaY){
+void SetA(int Nx, int Ny, double* a, double deltaX, double deltaY){
     double A[4][(Nx-2)*(Ny-2)];
     a = &A[0][0];
     
@@ -23,4 +23,19 @@ void SetA(int Nx, int Ny, double*a, double deltaX, double deltaY){
     for(int i=Nx-2; i<(Nx-2)*(Ny-2); i+=Nx-2){
         *(a + 3*(Ny - 2) + i) = 0.0;
     }
+}
+
+void SetY(int Nx, int Ny, double* y, double* v){
+    double Y[(Nx-2)*(Ny-2)];
+    int k = 0;
+    for(int j=1; j<Ny-1; j++){
+        for(int i=1; i<Nx-1; i++){
+            *(y + k) = *(v + j*Nx + i);//omega[i][j];
+            k ++;
+        }
+    }
+}
+
+void SetX(int Nx, int Ny, double* x){
+    double X[(Nx-2)*(Ny-2)];
 }
--- LidDrivenCavitySolver/PoissonSolver.h
+++ LidDrivenCavitySolver/PoissonSolver.h
@@ -13,10 +13,19 @@ public:
     ~PoissonSolver();
     
     //MEMBER FUNCTIONS
-    void SetA(int Nx, int Ny, double*a);
+    void SetA(int Nx, int Ny, double* a);
+    void SetY(int Nx, int Ny, double* y, double* v);
+    void SetX(int Nx, int Ny, double* x);
 
 private:
     double* a = nullptr;
+    double* y = nullptr;
+    double* x = nullptr;
 };
 
 #endif //POISSON_SOLVER_H
+
+/* Poisson Solver class used to solve a set of linear equations in the form y = Ax
+ * y = vector of known interior vorticities at time t+dt
+ * x = vector of unknown interior streamfunctions at time t+dt
+ * A = symmetric banded matrix of constant coefficients*/
